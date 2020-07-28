9b521b7174a615d33811bb040bc864c824558a3c
Rachel Chan <rachelchan1497@gmail.com>
2020-07-28 17:01:45 +0100

    post tests done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+const mongoose = require('mongoose');
 const db = require('./server/db/mongoose')
 const bookingsRouter = require('./server/routes/bookings-router')
 const app = express()
 const apiPort = 5000
+// *** config file *** //
+const config = require('./server/_config');
+
+// *** mongoose *** ///
+mongoose.connect(config.mongoURI[app.settings.env], function(err, res) {
+  if(err) {
+    console.log('Error connecting to the database. ' + err);
+  } else {
+    console.log('Connected to Database: ' + config.mongoURI[app.settings.env]);
+  }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -18,4 +29,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/cinema', bookingsRouter)
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+const server = app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+module.exports = server;
